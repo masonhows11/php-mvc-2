6de244f28ff8d2e9b1f7e3be8a2b11e6c700881f
mason.hows11 <mason.hows11@gmail.com>
2025-03-27 16:36:43 +0330

    attribute class updated

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -4,5 +4,30 @@ namespace System\Database\Traits;
 
 trait HasAttributes
 {
+    private function registerAttribute()
+    {
+        
+    }
 
+
+    protected function arrayToAttribute()
+    {
+        
+    }
+
+
+    protected function arrayToObjects()
+    {
+        
+    }
+
+    private function inHiddenAttributes()
+    {
+        
+    }
+
+    private function inCastAttributes()
+    {
+        
+    }
 }
