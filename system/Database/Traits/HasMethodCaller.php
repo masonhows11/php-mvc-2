625c08603f78293a70e41a37043a42888b211180
mason.hows11 <mason.hows11@gmail.com>
2025-03-29 23:12:29 +0330

    hasMethodcaller trait updated

--- system/Database/Traits/HasMethodCaller.php
+++ system/Database/Traits/HasMethodCaller.php
@@ -3,4 +3,24 @@ namespace System\Database\Traits;
 trait HasMethodCaller
 {
 
+
+    private array $allMethods =
+        ['create','update','delete','all','find','save',
+                           'where','whereOr','whereIn','whereNull','whereNotNull',
+                           'limit','limit','orderBy','get','paginate'];
+
+
+    // setAllowedMethods
+    private array $AllowedMethods = ['create','update','delete','all','find','save',
+        'where','whereOr','whereIn','whereNull','whereNotNull',
+        'limit','limit','orderBy','get','paginate'];
+
+
+
+    protected function  setAllowedMethods($array = []): void
+    {
+        $this->AllowedMethods =  $array ;
+    }
+
+
 }
