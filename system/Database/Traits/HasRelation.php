c752a2652ee0ac525ebd684bfe71a224c2d69d1d
mason.hows11 <mason.hows11@gmail.com>
2025-03-30 18:41:30 +0330

    hasRelation trait updated

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -4,4 +4,10 @@ namespace System\Database\Traits;
 trait HasRelation
 {
 
+    // relation between to model
+    // one to one & reverse
+    // one to many & reverse
+    // mann to many & reverse
+
+
 }
