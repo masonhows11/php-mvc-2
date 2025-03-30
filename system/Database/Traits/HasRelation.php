f0cff6b85ef01177594fa42f63a49c296660918b
mason.hows11 <mason.hows11@gmail.com>
2025-03-30 19:18:20 +0330

    hasRelation trait comment updated

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -24,8 +24,21 @@ trait HasRelation
 
     public function getHasOneRelation($table,$foreignKey,$otherKey,$otherKeyValue)
     {
+        // sql = 'select phones.* from users join  phones on users.id = phones.user_id';
+        // a left table like users
+        // b right table like phone
+        // user hasOne phone
 
-        
+        $this->setSql( "SELECT `b`.* FROM `{$table}` AS `a` JOIN ".$this->getTableName()." AS `b` ON `a`.`{$otherKey}` = `b`.`{$foreignKey}` ");
+        $this->setWhere('AND',"`a`.`{$otherKey}` = ? ");
+        $this->table = 'b';
+        $this->addValue($otherKey,$otherKeyValue);
+        $statement = $this->executeQuery();
+        $data = $statement->fetch();
+        if($data){
+            return $this->arrayToAttributes($data);
+        }
+        return null;
 
     }
 
