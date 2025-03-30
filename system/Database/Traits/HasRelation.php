11d919c669345c7c37ad630ff9f7a3691594602f
mason.hows11 <mason.hows11@gmail.com>
2025-03-30 18:59:38 +0330

    hasRelation trait comment updated

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -4,13 +4,30 @@ namespace System\Database\Traits;
 trait HasRelation
 {
 
-    // relation between two  model / models
+    // relation between two  model/table or models/tables
     // one to one & reverse
     // one to many & reverse
     // mann to many & reverse
 
 
-    
+    protected function hasOne($model,$foreignKey,$localKey)
+    {
+
+        if($this->{$this->primaryKey}){
+
+            $modelObject = new $model;
+            return $modelObject->getHasOneRelation($this->table,$foreignKey,$localKey,$this->$localKey);
+        }
+        return null;
+    }
+
+
+    public function getHasOneRelation($table,$foreignKey,$otherKey,$otherKeyValue)
+    {
+
+        
+
+    }
 
 
 }
