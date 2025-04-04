a2dfca9643bcd78006c1fd37259c6fa305dc6d8d
mason.hows11 <mason.hows11@gmail.com>
2025-04-04 21:09:46 +0330

    migrations updated

--- system/Database/DbBuilder/DbBuilder.php
+++ system/Database/DbBuilder/DbBuilder.php
@@ -2,7 +2,31 @@
 
 namespace System\Database\DbBuilder;
 
+use System\Database\DBConnection\DBConnection;
+
+
 class DbBuilder
 {
+
+
+    public function __construct()
+    {
+
+        $this->createTables();
+        die("migrations run successfully");
+
+    }
+
+
+    private function createTables()
+    {
+        $migrations = $this->getMigrations();
+
+        
+
+    }
+
+
+
 
 }
