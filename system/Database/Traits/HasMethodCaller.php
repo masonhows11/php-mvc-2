aeee284e42485d920b4d2513a9f39cf1adb472c2
mason.hows11 <mason.hows11@gmail.com>
2025-04-04 09:41:46 +0330

    methodCaller has problem

--- system/Database/Traits/HasMethodCaller.php
+++ system/Database/Traits/HasMethodCaller.php
@@ -1,50 +1,55 @@
 <?php
+
 namespace System\Database\Traits;
 trait HasMethodCaller
 {
 
 
     private array $allMethods =
-        ['create','update','delete','all','find','save',
-                           'where','whereOr','whereIn','whereNull','whereNotNull',
-                           'limit','limit','orderBy','get','paginate'];
+        ['create', 'update', 'delete', 'all', 'find', 'save',
+            'where', 'whereOr', 'whereIn', 'whereNull', 'whereNotNull',
+            'limit', 'limit', 'orderBy', 'get', 'paginate'];
 
 
     // setAllowedMethods
-    private array $AllowedMethods = ['create','update','delete','all','find','save',
-        'where','whereOr','whereIn','whereNull','whereNotNull',
-        'limit','limit','orderBy','get','paginate'];
+    private array $AllowedMethods = ['create', 'update', 'delete', 'all', 'find', 'save',
+        'where', 'whereOr', 'whereIn', 'whereNull', 'whereNotNull',
+        'limit', 'limit', 'orderBy', 'get', 'paginate'];
 
 
     //// use magic method for call / execute doesn't exist method
     public function __call(string $method, array $arguments)
     {
-        return $this->methodCaller($this,$method,$arguments);
+        return $this->methodCaller($this, $method, $arguments);
     }
 
     public static function __callStatic(string $method, array $arguments)
     {
-       // use like this user::find(1)
+        // use like this user::find(1)
         $className = get_called_class();
         $instance = new $className;
-        return $instance->methodCaller($instance,$method,$arguments);
+        return $instance->methodCaller($instance, $method, $arguments);
     }
 
-    protected function methodCaller($object,$method,$args)
+    protected function methodCaller($object, $method, $args)
     {
         $suffix = 'Method';
-        $methodName = $method.$suffix;
+        $methodName = $method . $suffix;
 
-        if(in_array($methodName,$this->AllowedMethods)){
-          // return means : return the result of current method to next method
-          return  call_user_func_array(array($object,$methodName),$args);
+        // return $methodName;
+        if (in_array($methodName, $this->AllowedMethods)) {
+            // return means : return the result of current method to next method
+            return call_user_func_array(array($object, $methodName), $args);
+        } else {
+            return "$methodName does not exists";
         }
+
     }
 
 
-    protected function  setAllowedMethods($array = []): void
+    protected function setAllowedMethods($array): void
     {
-        $this->AllowedMethods =  $array ;
+        $this->AllowedMethods = $array;
     }
 
 
