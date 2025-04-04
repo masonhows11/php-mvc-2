b67038c17d0ecc36856f6a4fd827c2bacc8d6127
mason.hows11 <mason.hows11@gmail.com>
2025-04-04 09:53:48 +0330

    query builder updated

--- system/Database/DBConnection/DBConnection.php
+++ system/Database/DBConnection/DBConnection.php
@@ -26,15 +26,14 @@ class DBConnection
         return self::$dbConnectionInstance;
     }
 
-    private function dbConnection() : array
+    private function dbConnection(): false|\PDO
     {
         // config pdo options
         $options = array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC);
         try {
 
             return  new \PDO("mysql:host=".DBHOST.";dbname=".DBNAME,DBUSERNAME,DBPASSWORD,$options);
-
-
+            
         }catch (\PDOException $exception){
 
             echo "error in database connection: ".$exception->getMessage();
