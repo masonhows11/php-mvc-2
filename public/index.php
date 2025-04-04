0c7d0a0fb247041c3f4ccdc9fe0d627095cd56bd
mason.hows11 <mason.hows11@gmail.com>
2025-04-04 07:46:54 +0330

    updated

--- public/index.php
+++ public/index.php
@@ -2,10 +2,9 @@
 
 // dir(__DIR__) its root path
 // then vendor /dir then /autoload.php file
-// var_dump('hi');
+
 require_once (dirname(__DIR__)."/vendor/autoload.php");
 
 require_once ("../bootstrap/app.php");
 
 
-// echo "<br/><br/>current_route: " .CURRENT_ROUTE ."<br/><br/>";
