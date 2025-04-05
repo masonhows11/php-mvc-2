89bdbd9a3e5981ad63feb03488f7e1b9307914cf
mason.hows11 <mason.hows11@gmail.com>
2025-04-05 16:37:58 +0330

    Request class updated

--- system/Request/Request.php
+++ system/Request/Request.php
@@ -6,24 +6,51 @@ namespace System\Request;
 use System\Request\Traits\HasFileValidationRules;
 use System\Request\Traits\HasRunValidation;
 use System\Request\Traits\HasValidationRules;
+
 class Request
 {
 
-    use HasValidationRules,HasRunValidation,HasFileValidationRules;
+    use HasValidationRules, HasRunValidation, HasFileValidationRules;
 
 
     protected bool $errorExist = false;
 
-    protected $request;
+    protected array $request;
 
-    protected $files = null;
+    protected array $files;
 
     protected array $errorVariableName = [];
 
 
     public function __construct()
     {
-        
+        if (isset($_POST))
+        {
+            $this->postAttribute();
+        }
+        if(!empty($_FILES))
+        {
+            $this->files = $_FILES;
+        }
+    }
+
+    public function file($name)
+    {
+        return isset($this->files[$name]) ? $this->files[$name] : false;
+    }
+
+    protected function postAttribute()
+    {
+        // key -> value = first_name : hassan
+        foreach ($_POST as $key => $value) {
+            $this->$key = htmlentities($value);
+            $this->request[$key] = htmlentities($value);
+        }
+    }
+
+    public function all()
+    {
+        return $this->request;
     }
 
 }
