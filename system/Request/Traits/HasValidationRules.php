c186bca99c48370475d4601a2aeb9f5d06300d5f
mason.hows11 <mason.hows11@gmail.com>
2025-04-05 23:16:16 +0330

    HasvalidationRules exists method updated

--- system/Request/Traits/HasValidationRules.php
+++ system/Request/Traits/HasValidationRules.php
@@ -56,28 +56,24 @@ trait HasValidationRules
 
                 $this->required($name);
 
-            } elseif (strpos($rule, "max:") === 0)
-            {
+            } elseif (strpos($rule, "max:") === 0) {
 
                 $rule = str_replace('max:', "", $rule);
                 $this->maxNumber($name, $rule);
 
-            } elseif (strpos($rule, "min:") === 0)
-            {
+            } elseif (strpos($rule, "min:") === 0) {
 
                 $rule = str_replace('min:', "", $rule);
                 $this->minNumber($name, $rule);
 
-            } elseif (strpos($rule, "exists:") === 0)
-            {
+            } elseif (strpos($rule, "exists:") === 0) {
 
                 $rule = str_replace('exists:', "", $rule);
                 $rule = explode(',', $rule);
                 $key = isset($rule[1]) == false ? null : $rule[1];
                 $this->existsIn($name, $rule[0], $key);
 
-            } elseif ($rule == 'number')
-            {
+            } elseif ($rule == 'number') {
 
                 $this->number($name);
 
@@ -86,58 +82,54 @@ trait HasValidationRules
 
         }
     }
+
     protected function required($name)
     {
         // $name -> is name field come from post request
         // like name title age phone
-        if ((!isset($this->request[$name]) or $this->request[$name] === '' ) && $this->checkFirstError($name)){
+        if ((!isset($this->request[$name]) or $this->request[$name] === '') && $this->checkFirstError($name)) {
 
-            $this->setError($name,"$name is required");
+            $this->setError($name, "$name is required");
         }
     }
 
 
     //// number/integer methods validation
-    protected function maxNumber($name,$count): void
+    protected function maxNumber($name, $count): void
     {
-        if($this->checkFieldExists($name)){
+        if ($this->checkFieldExists($name)) {
 
-            if($this->request[$name] >= $count && $this->checkFirstError($name))
-            {
-                $this->setError($name,"max number equal or lower than $count");
+            if ($this->request[$name] >= $count && $this->checkFirstError($name)) {
+                $this->setError($name, "max number equal or lower than $count");
             }
         }
     }
 
-    protected function minNumber($name,$count): void
+    protected function minNumber($name, $count): void
     {
-        if($this->checkFieldExists($name)){
+        if ($this->checkFieldExists($name)) {
 
-            if($this->request[$name] <= $count && $this->checkFirstError($name))
-            {
-                $this->setError($name,"min number equal or upper than $count");
+            if ($this->request[$name] <= $count && $this->checkFirstError($name)) {
+                $this->setError($name, "min number equal or upper than $count");
             }
         }
     }
 
     protected function number($name)
     {
-        if($this->checkFieldExists($name))
-        {
-            if(!is_numeric($this->request[$name]) && $this->checkFirstError($name))
-            {
-                $this->setError($name,"$name must be number format");
+        if ($this->checkFieldExists($name)) {
+            if (!is_numeric($this->request[$name]) && $this->checkFirstError($name)) {
+                $this->setError($name, "$name must be number format");
             }
         }
     }
 
     protected function date($name)
     {
-        if($this->checkFieldExists($name))
-        {   // 1399-02-22 -> /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/
-            if(!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$this->request[$name])){
+        if ($this->checkFieldExists($name)) {   // 1399-02-22 -> /^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/
+            if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $this->request[$name])) {
 
-                $this->setError($name,"$name not valid date format");
+                $this->setError($name, "$name not valid date format");
             };
 
         }
@@ -145,44 +137,52 @@ trait HasValidationRules
 
     protected function email($name)
     {
-        if($this->checkFieldExists($name))
-        {
-            if(!filter_var($this->request[$name], FILTER_VALIDATE_EMAIL) && $this->checkFirstError($name))
-            {
-                $this->setError($name,"$name not valid email format");
+        if ($this->checkFieldExists($name)) {
+            if (!filter_var($this->request[$name], FILTER_VALIDATE_EMAIL) && $this->checkFirstError($name)) {
+                $this->setError($name, "$name not valid email format");
             }
         }
     }
-
-
 
 
     //// string methods validation
-    protected function maxStr($name,$count): void
+    protected function maxStr($name, $count): void
     {
-        if($this->checkFieldExists($name)){
+        if ($this->checkFieldExists($name)) {
 
-            if(strlen($this->request[$name]) >= $count && $this->checkFirstError($name))
-            {
-                $this->setError($name,"max length equal or lower than $count characters");
+            if (strlen($this->request[$name]) >= $count && $this->checkFirstError($name)) {
+                $this->setError($name, "max length equal or lower than $count characters");
             }
         }
     }
 
-    protected function minStr($name,$count): void
+    protected function minStr($name, $count): void
     {
-        if($this->checkFieldExists($name)){
+        if ($this->checkFieldExists($name)) {
 
-            if(strlen($this->request[$name]) <= $count && $this->checkFirstError($name))
-            {
-                $this->setError($name,"min length equal or upper than $count characters");
+            if (strlen($this->request[$name]) <= $count && $this->checkFirstError($name)) {
+                $this->setError($name, "min length equal or upper than $count characters");
             }
         }
     }
 
 
-    protected function existsIn()
+    protected function existsIn($name, $table, $field_id = "id")
     {
+        if ($this->checkFieldExists($name)) {
+            if ($this->checkFirstError($name)) {
 
+                $value = $this->$name;
+                $sql = "SELECT COUNT(*) FROM $table WHERE $field_id = ? ";
+                $statement = DBConnection::getDbConnectionInstance()->prepare($sql);
+                $statement->execute([$value]);
+                $result = $statement->fetchColumn();
+                if($result == 0 or $result === false)
+                {
+                    $this->setError($name, "$name does not exists");
+                }
+
+            }
+        }
     }
 }
