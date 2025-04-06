a1d166b0ee260b3901b632f42307dd9e10b9e8a1
mason.hows11 <mason.hows11@gmail.com>
2025-04-06 17:48:06 +0330

    HasFilevalidationRules trait methods updated

--- system/Request/Traits/HasFileValidationRules.php
+++ system/Request/Traits/HasFileValidationRules.php
@@ -9,18 +9,17 @@ trait HasFileValidationRules
 
     // $name id field name
     // rulesArray is array of rule validation like required,max,min
-    protected function fileValidation($name,$rulesArray)
+    protected function fileValidation($name, $rulesArray)
     {
-        foreach ($rulesArray as $rule){
+        foreach ($rulesArray as $rule) {
 
-            if($rule == "required")
-            {
+            if ($rule == "required") {
                 $this->fileRequired($name);
 
             } elseif (strpos($rule, "mimes:") === 0) {
 
                 $rule = str_replace('mimes:', "", $rule);
-                $rule = explode(',',$rule);
+                $rule = explode(',', $rule);
                 $this->fileType($name, $rule);
 
             } elseif (strpos($rule, "max:") === 0) {
@@ -28,7 +27,7 @@ trait HasFileValidationRules
                 $rule = str_replace('max:', "", $rule);
                 $this->maxFile($name, $rule);
 
-            }elseif (strpos($rule, "min:") === 0) {
+            } elseif (strpos($rule, "min:") === 0) {
 
                 $rule = str_replace('min:', "", $rule);
                 $this->minFile($name, $rule);
@@ -36,5 +35,29 @@ trait HasFileValidationRules
             }
         }
     }
-    
+
+
+    protected function fileRequired($name): void
+    {
+
+        if (!isset($this->files[$name]['name']) || empty($this->files[$name]['name']) && $this->checkFirstError($name)) {
+            $this->setError($name, "$name is required");
+        }
+    }
+
+    protected function fileType($name,$typeArray)
+    {
+
+    }
+
+    protected function MaxFile($name,$size)
+    {
+
+    }
+
+    protected function minFile($name,$size)
+    {
+
+    }
+
 }
