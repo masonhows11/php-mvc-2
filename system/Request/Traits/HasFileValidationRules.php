e7972ff362545c122c3cedfb87571ebce8451e6e
mason.hows11 <mason.hows11@gmail.com>
2025-04-06 17:15:00 +0330

    HasFilevalidationRules trait updated

--- system/Request/Traits/HasFileValidationRules.php
+++ system/Request/Traits/HasFileValidationRules.php
@@ -2,7 +2,39 @@
 
 namespace System\Request\Traits;
 
+
 trait HasFileValidationRules
 {
 
+
+    // $name id field name
+    // rulesArray is array of rule validation like required,max,min
+    protected function fileValidation($name,$rulesArray)
+    {
+        foreach ($rulesArray as $rule){
+
+            if($rule == "required")
+            {
+                $this->fileRequired($name);
+
+            } elseif (strpos($rule, "mimes:") === 0) {
+
+                $rule = str_replace('mimes:', "", $rule);
+                $rule = explode(',',$rule);
+                $this->fileType($name, $rule);
+
+            } elseif (strpos($rule, "max:") === 0) {
+
+                $rule = str_replace('max:', "", $rule);
+                $this->maxFile($name, $rule);
+
+            }elseif (strpos($rule, "min:") === 0) {
+
+                $rule = str_replace('min:', "", $rule);
+                $this->minFile($name, $rule);
+
+            }
+        }
+    }
+    
 }
