767eea573e68661687998eedb9df12b8d171827f
mason.hows11 <mason.hows11@gmail.com>
2025-04-08 15:50:48 +0330

    auth class updated

--- system/Auth/Auth.php
+++ system/Auth/Auth.php
@@ -9,4 +9,24 @@ use System\Session\Session;
 class Auth
 {
 
+
+    public function __call(string $name, array $arguments)
+    {
+       return $this->methodCaller($name,$arguments);
+    }
+
+    public static function __callStatic(string $name, array $arguments)
+    {
+       $instance = new self;
+       return $instance->methodCaller($name,$arguments);
+    }
+
+    protected function methodCaller($method, $args)
+    {
+        $suffix = 'Method';
+        $methodName = $method.$suffix;
+
+        return call_user_func_array(array($this,$methodName), $args);
+
+    }
 }
