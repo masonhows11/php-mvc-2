e7b7f2b96a2224440cd1f682d0af40769f538238
mason.hows11 <mason.hows11@gmail.com>
2025-04-08 18:39:20 +0330

    view sys updated

---
+++ system/View/Traits/ViewLoader.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace System\View\Traits;
+
+trait ViewLoader
+{
+
+}
---
+++ system/View/ViewBuilder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace System\View;
+
+use System\View\Traits\ViewLoader;
+class ViewBuilder
+{
+    use ViewLoader;
+
+    public function run($path)
+    {
+        
+    }
+
+}
