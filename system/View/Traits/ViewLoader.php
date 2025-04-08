f3ca1c50a5e3879dd9d8a91fb5362ce7f2bae2f3
mason.hows11 <mason.hows11@gmail.com>
2025-04-08 18:41:02 +0330

    view sys updated

--- system/View/Traits/ViewLoader.php
+++ system/View/Traits/ViewLoader.php
@@ -4,5 +4,19 @@ namespace System\View\Traits;
 
 trait ViewLoader
 {
+    private array $viewNameArray = [];
 
+
+    private function ViewLoader($path)
+    {
+        
+    }
+
+
+    private function registerView($view)
+    {
+
+    }
+
+    
 }
