bf81165f3ca3ffcad0ef99a3d029ba38a775eb1f
mason.hows11 <mason.hows11@gmail.com>
2025-04-08 15:58:38 +0330

    auth class methods updated

--- system/Auth/Auth.php
+++ system/Auth/Auth.php
@@ -10,23 +10,54 @@ class Auth
 {
 
 
+    private string $redirectTo = '/login';
+
+
+    private function userMethod()
+    {
+        if (!Session::get('user')) {
+            return redirect($this->redirectTo);
+        }
+
+        $user = User::find(Session::get('user'));
+
+        if (empty($user))
+        {
+            Session::remove('user');
+            return redirect($this->redirectTo);
+
+        } else {
+
+            return $user;
+        }
+
+
+    }
+
+
+    private function idMethod()
+    {
+
+    }
+
+
     public function __call(string $name, array $arguments)
     {
-       return $this->methodCaller($name,$arguments);
+        return $this->methodCaller($name, $arguments);
     }
 
     public static function __callStatic(string $name, array $arguments)
     {
-       $instance = new self;
-       return $instance->methodCaller($name,$arguments);
+        $instance = new self;
+        return $instance->methodCaller($name, $arguments);
     }
 
     protected function methodCaller($method, $args)
     {
         $suffix = 'Method';
-        $methodName = $method.$suffix;
+        $methodName = $method . $suffix;
 
-        return call_user_func_array(array($this,$methodName), $args);
+        return call_user_func_array(array($this, $methodName), $args);
 
     }
 }
