ac3ef29bdebd398a0d39275894b03e792831612c
mason.hows11 <mason.hows11@gmail.com>
2025-04-09 20:49:52 +0330

    composer trait updated

--- system/View/Composer.php
+++ system/View/Composer.php
@@ -16,16 +16,43 @@ class Composer
 
     }
 
+    private function registerView($name, $callback): void
+    {
+        if (in_array(str_replace('.', '/', '$name'), $this->viewArray) || $name == '*') {
+            // $callback() get variables we want pass to view/view's in composer by $callback()
+            // then put them to $viewVars
+            $viewVars = $callback();
 
-    private function setViewArray($viewArray)
+            foreach ($viewVars as $key => $value) {
+
+                    $this->vars[$key] = $value;
+                    //// store below data as key->value into $vars
+                    //// pass these variables to view/views
+                    //      "sumArea"       => $sumArea,
+                    //      "usersCount"    => $usersCount,
+                    //      "adsCount"      => count($ads),
+                    //      "postsCount"    => $postsCount
+            }
+
+        }
+
+    }
+
+
+    private function setViewArray($viewArray): void
     {
         $this->viewArray = $viewArray;
 
     }
+
+    private function getViewVars(): array
+    {
+        return $this->vars;
+    }
+
     private static function getInstance()
     {
-        if(empty(self::$instance))
-        {
+        if (empty(self::$instance)) {
             self::$instance = new self;
             return self::$instance;
         }
