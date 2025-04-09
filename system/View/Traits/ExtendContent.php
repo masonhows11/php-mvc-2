9c25d6d8ec42b735213e5b1272bb93b36cb4a47e
mason.hows11 <mason.hows11@gmail.com>
2025-04-09 13:36:43 +0330

    extends view updated

--- system/View/Traits/ExtendContent.php
+++ system/View/Traits/ExtendContent.php
@@ -11,11 +11,15 @@ trait ExtendContent
     private $extendsContent;
 
 
+    /**
+     * @throws \Exception
+     */
     private function checkExtendsContent()
     {
         $layoutsFilePath = $this->findExtends();
         if($layoutsFilePath){
 
+            $this->extendsContent = $this->viewLoader($layoutsFilePath);
         }
     }
 
@@ -26,6 +30,16 @@ trait ExtendContent
 
         // to use extends method or not
         preg_match("/s*@extends+\('([^)]+)'\)/",$this->content,$filePathArray);
+
+        return isset($filePathArray[1]) ? $filePathArray[1] : false;
+    }
+
+    private function findYieldsNames(): null|false
+    {
+        $yieldsNamesArray = [];
+
+        // to use extends method or not
+        preg_match_all("/s*@yield+\('([^)]+)'\)/",$this->extendsContent,$yieldsNamesArray);
 
         return isset($filePathArray[1]) ? $filePathArray[1] : false;
     }
