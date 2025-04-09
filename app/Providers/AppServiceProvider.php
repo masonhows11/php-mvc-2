be82c6a6d9eb15b485fee8f34881392a0f7d39c4
mason.hows11 <mason.hows11@gmail.com>
2025-04-09 19:32:13 +0330

    add appservice provider updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,12 +6,32 @@ use App\Models\User;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\Ads;
+use System\View\Composer;
+
 class AppServiceProvider extends Provider
 {
 
 
     public function boot()
     {
+
+
+        Composer::view("app.index", function (){
+            $ads = Ads::all();
+            $sumArea = 0;
+            foreach ($ads as $advertise)
+            {
+                $sumArea += (int) $advertise->area;
+            }
+            $usersCount = count(User::all());
+            $postsCount = count(Post::all());
+            return [
+                "sumArea"       => $sumArea,
+                "usersCount"    => $usersCount,
+                "adsCount"      => count($ads),
+                "postsCount"    => $postsCount
+            ];
+        });
 
     }
 
