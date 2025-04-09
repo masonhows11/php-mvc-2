ca428b83d48d4afc199a7b8065d71f8263781101
mason.hows11 <mason.hows11@gmail.com>
2025-04-09 14:00:22 +0330

    extends view config yield updated

--- system/View/Traits/ExtendContent.php
+++ system/View/Traits/ExtendContent.php
@@ -20,6 +20,16 @@ trait ExtendContent
         if($layoutsFilePath){
 
             $this->extendsContent = $this->viewLoader($layoutsFilePath);
+            $yieldsNamesArray = $this->findYieldsNames();
+
+            if($yieldsNamesArray){
+                foreach ($yieldsNamesArray as $yieldsName)
+                {
+                    $this->initialYields($yieldsName);
+                }
+            }
+
+            $this->content = $this->extendsContent;
         }
     }
 
@@ -31,7 +41,8 @@ trait ExtendContent
         // to use extends method or not
         preg_match("/s*@extends+\('([^)]+)'\)/",$this->content,$filePathArray);
 
-        return isset($filePathArray[1]) ? $filePathArray[1] : false;
+        //  return isset($filePathArray[1]) ? $filePathArray[1] : false;
+        return $filePathArray[1] ?? false;
     }
 
     private function findYieldsNames(): null|false
@@ -41,8 +52,27 @@ trait ExtendContent
         // to use extends method or not
         preg_match_all("/s*@yield+\('([^)]+)'\)/",$this->extendsContent,$yieldsNamesArray,PREG_UNMATCHED_AS_NULL);
 
-        return isset($yieldsNamesArray[1]) ? $yieldsNamesArray[1] : false;
+        // return isset($yieldsNamesArray[1]) ? $yieldsNamesArray[1] : false;
+        return $yieldsNamesArray[1] ?? false;
     }
 
+
+    private function initialYields($yieldsName)
+    {
+        $string = $this->content;
+        $startWord = "@section('" . $yieldsName . "')";
+        $endWord = "@endsection";
+
+
+        // find yield value in child view
+        // if yield parent is define in extends view
+        // example @yield('content') -> @section('content')
+        $startPos = strpos($string,$startWord);
+        if($startPos === false){
+
+            // remove @yield from extends view
+            return $this->extendsContent = str_replace("@yield('$yieldsName')","",$this->extendsContent);
+        }
+    }
 
 }
