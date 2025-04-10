5aa5eb415b7a0613808be64110d7bd717abfdfa3
mason.hows11 <mason.hows11@gmail.com>
2025-04-10 16:55:22 +0330

    config updated

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ return [
     'APP_TITLE' => 'mvc project',
 
     'BASE_URL' => 'http://localhost:8000',
-    
+
     define("BASE_DIR", realpath(__DIR__ . "/../")),
     // 'BASE_DIR' => dirname(__DIR__),
 
@@ -21,18 +21,18 @@ return [
 // get current route name
 // $raw_uri = explode('?', $_SERVER['REQUEST_URI'][0]);
 
-$temporary_url = str_replace(BASE_URL, '', explode('?', $_SERVER['REQUEST_URI'])[0]);
-
-$temporary_url === "/" ? $temporary_url = "" : $temporary_url = substr($temporary_url, 1);
-
-define("CURRENT_ROUTE", $temporary_url);
-
-
-global $routes;
-
-$routes = [
-    'get' => [],
-    'post' => [],
-    'put' => [],
-    'delete' => [],
-];
+//$temporary_url = str_replace(BASE_URL, '', explode('?', $_SERVER['REQUEST_URI'])[0]);
+//
+//$temporary_url === "/" ? $temporary_url = "" : $temporary_url = substr($temporary_url, 1);
+//
+//define("CURRENT_ROUTE", $temporary_url);
+//
+//
+//global $routes;
+//
+//$routes = [
+//    'get' => [],
+//    'post' => [],
+//    'put' => [],
+//    'delete' => [],
+//];
