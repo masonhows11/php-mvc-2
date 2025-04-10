d61d8f5591f26536c20bb003c67c3433615181f6
mason.hows11 <mason.hows11@gmail.com>
2025-04-10 17:24:57 +0330

    config class make access array items updated

--- system/Config/Config.php
+++ system/Config/Config.php
@@ -5,6 +5,15 @@ namespace System\Config;
 class Config
 {
 
+
+    private static $instance;
+
+    private array $config_nested_array = [];
+    private array $config_dot_array = [];
+
+
+
+
     
 
 }
